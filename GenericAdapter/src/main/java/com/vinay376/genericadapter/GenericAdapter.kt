37bbbade3d174157dd1b916f6f56f61d2fbf4b49
VinayKumarParas <vinay.parastechnologies.in>
2022-01-11 00:44:52 +0530

    Add Generic Adapter

---
+++ GenericAdapter/src/main/java/com/vinay376/genericadapter/GenericAdapter.kt
@@ -0,0 +1,55 @@
+package com.vinay376.genericadapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+
+/** <T> is used for Layout Type Binding */
+/** <M> is used for Data Class for specific type */
+/** Pass a <M> in DiffUtil Class */
+
+abstract class GenericAdapter<T, M> :
+    ListAdapter<M, RecyclerView.ViewHolder>(GenericDiffUtil<M>()) {
+
+    lateinit var binding: ViewDataBinding
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        binding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            getResourceLayoutId(),
+            parent,
+            false
+        )
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        try {
+            holder as ViewHolder
+            holder.binding.root.setAnimation()
+            val dataClass = getItem(position)
+            onBindHolder(holder.binding as T, dataClass)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    class ViewHolder(val binding: ViewDataBinding) : RecyclerView.ViewHolder(binding.root)
+
+    abstract fun getResourceLayoutId(): Int
+
+    abstract fun onBindHolder(holder: T, dataClass: M)
+    
+    /** Animation Function */
+    private fun View.setAnimation() {
+//        val animation: Animation = AnimationUtils.loadAnimation(this.context, R.anim.recycler_animation)
+//        this.startAnimation(animation)
+    }
+
+
+}
---
+++ GenericAdapter/src/main/java/com/vinay376/genericadapter/GenericDiffUtil.kt
@@ -0,0 +1,22 @@
+package com.vinay376.genericadapter
+
+import android.annotation.SuppressLint
+import androidx.recyclerview.widget.DiffUtil
+
+/** You pass <M> data class type
+ * and its received in <T> data
+ * class type.
+ * */
+
+class GenericDiffUtil<T> : DiffUtil.ItemCallback<T>() {
+    /** Check old item and new item are same
+     *  If they are both same */
+    override fun areItemsTheSame(oldItem: T, newItem: T): Boolean {
+        return oldItem == newItem
+    }
+
+    @SuppressLint("DiffUtilEquals")
+    override fun areContentsTheSame(oldItem: T, newItem: T): Boolean {
+        return oldItem == newItem
+    }
+}
